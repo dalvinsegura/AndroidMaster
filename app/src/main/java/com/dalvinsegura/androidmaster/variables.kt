fd64d85254d042d0b153ca8716f0a671c4c04378
Dalvin Segura <byjacson18@gmail.com>
2023-09-16 21:48:40 -0400

    Initial commit

---
+++ app/src/main/java/com/dalvinsegura/androidmaster/variables.kt
@@ -0,0 +1,32 @@
+package com.dalvinsegura.androidmaster
+
+//VARIABLES
+
+fun main(){
+    //Int: small integer
+    val age:Int = 30
+
+    //Long: long numbers
+    val example:Long = 5454553453
+
+    //Float: float numbers (it's important the letter "f" at the end, then Android will ignore it).
+    val floatExample:Float = 30.3f
+
+    //Double
+    val doubleExample:Double = 33.3434343
+
+//Alphanumeric Variables
+
+    // Char: only support one character. It can save any character like number, letters or symbol...
+    val charExample1:Char = 'f'
+
+    // String: doesn't has a character limit.
+    val stringCharater:String = "Hi, Dalvin"
+
+    // Boolean
+    val booleanExample:Boolean = true
+
+
+    print(stringCharater);
+
+}
