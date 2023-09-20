ff4c889c711d10737f2f4efa1211dbcd8e66de29
Dalvin Segura <byjacson18@gmail.com>
2023-09-20 12:05:15 -0400

    I already know some new components and about intents. In details: - Greeting App: In this update i've done an app where you can introduce your name and click a button and after that if there's a value introduced will use the intent to put the name and being used in a new screen.

--- app/src/main/java/com/dalvinsegura/androidmaster/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/dalvinsegura/androidmaster/firstapp/FirstAppActivity.kt
@@ -1,13 +1,33 @@
 package com.dalvinsegura.androidmaster.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.dalvinsegura.androidmaster.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_first_app)
-        setContentView(R.layout.constrains_layout_lesson)
+        setContentView(R.layout.activity_first_app)
+//        setContentView(R.layout.constrains_layout_lesson)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+
+
+        btnStart.setOnClickListener{
+            val name = etName.text.toString()
+        if (name.isNotEmpty()){
+        val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("EXTRA_NAME", name)
+            startActivity(intent)
+
+        }
+        }
+
     }
 }
