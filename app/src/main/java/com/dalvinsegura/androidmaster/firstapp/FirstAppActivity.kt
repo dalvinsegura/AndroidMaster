708a34816cb5d47c0fc9e72000dfae00961ad5f6
Dalvin Segura <byjacson18@gmail.com>
2023-09-19 20:14:23 -0400

    I learnt about constrains layout and built a login view (a basic one).

--- app/src/main/java/com/dalvinsegura/androidmaster/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/dalvinsegura/androidmaster/firstapp/FirstAppActivity.kt
@@ -7,6 +7,7 @@ import com.dalvinsegura.androidmaster.R
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_app)
+//        setContentView(R.layout.activity_first_app)
+        setContentView(R.layout.constrains_layout_lesson)
     }
 }
