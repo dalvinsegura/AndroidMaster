76fbc6ff9f5719a1f854655799ba240ff33ec57b
Dalvin Segura <byjacson18@gmail.com>
2023-09-21 09:28:01 -0400

    I created a new activity simulating a menu, this one will navigate to an existing activity when the button is pulsed.

---
+++ app/src/main/java/com/dalvinsegura/androidmaster/MenuActivity.kt
@@ -0,0 +1,21 @@
+package com.dalvinsegura.androidmaster
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.dalvinsegura.androidmaster.firstapp.FirstAppActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        btnSaludApp.setOnClickListener{navigateToSaludApp()}
+    }
+
+    private fun navigateToSaludApp(){
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/dalvinsegura/androidmaster/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/dalvinsegura/androidmaster/firstapp/FirstAppActivity.kt
@@ -1,5 +1,6 @@
 package com.dalvinsegura.androidmaster.firstapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,7 @@ import androidx.appcompat.widget.AppCompatEditText
 import com.dalvinsegura.androidmaster.R
 
 class FirstAppActivity : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
