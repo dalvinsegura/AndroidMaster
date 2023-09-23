f17324b6c12218855fac42a61ee7ac5f39181805
Dalvin Segura <byjacson18@gmail.com>
2023-09-22 22:41:05 -0400

    I'm building an app that calculate the BMI (body mass index), so far, i have only the layout with 2 gender card view without any action.
    In this new change i learn about:
    - String resources (its importance of use)
    - Using SVG asset. Importing or using the local SVG.
    - CardView Component.
    - ImageView Component.
    - CornerRadius Attribute.
    - Usage of LinearLayout and ConstraintLayout nested.

--- app/src/main/java/com/dalvinsegura/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/dalvinsegura/androidmaster/MenuActivity.kt
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.dalvinsegura.androidmaster.firstapp.FirstAppActivity
+import com.dalvinsegura.androidmaster.imccalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnImcCalculatorApp = findViewById<Button>(R.id.btnImcCalculatorApp)
         btnSaludApp.setOnClickListener{navigateToSaludApp()}
+        btnImcCalculatorApp.setOnClickListener{navigateToImcApp()}
+    }
+
+    private fun navigateToImcApp(){
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
     }
 
     private fun navigateToSaludApp(){
