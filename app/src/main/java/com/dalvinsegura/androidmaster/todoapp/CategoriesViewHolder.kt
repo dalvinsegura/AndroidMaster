81e137462d72a1a1dd080336a78ca2096661c19c
Dalvin Segura <byjacson18@gmail.com>
2023-09-29 22:20:46 -0400

    In this update, I'v created some new classes for Recycle View managment and basic layout for the Recycle view card.

---
+++ app/src/main/java/com/dalvinsegura/androidmaster/todoapp/CategoriesViewHolder.kt
@@ -0,0 +1,8 @@
+package com.dalvinsegura.androidmaster.todoapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoriesViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+}
--- app/src/main/java/com/dalvinsegura/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/dalvinsegura/androidmaster/todoapp/TodoActivity.kt
@@ -2,11 +2,24 @@ package com.dalvinsegura.androidmaster.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.dalvinsegura.androidmaster.R
 
 class TodoActivity : AppCompatActivity() {
+
+    private lateinit var rvCategories: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+        initComponents()
+        initUI()
+    }
+
+    private fun initComponents() {
+        rvCategories = findViewById(R.id.rvCategories)
+    }
+
+    private fun initUI() {
+
     }
 }
